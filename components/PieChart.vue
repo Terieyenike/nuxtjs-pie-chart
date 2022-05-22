689d8d7a6addf467b8dfd614bc675d1f0872877f
Oteri Eyenike <teyenike1@gmail.com>
2022-05-22 20:45:56 +0100

    Add: pie chart using datasets

---
+++ components/PieChart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1 class="text-2xl">{{ text }}</h1>
+    <Pie :data="chartData"/>
+  </div>
+</template>
+
+<script>
+import {Pie} from 'vue-chartjs'
+
+const DATA_COUNT = 5;
+const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+
+export default {
+  components: {
+    Pie
+  },
+  data() {
+    return {
+      text: "Pie chart",
+      chartData: {
+        labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+        // datasets: [
+        //   {
+        //     label: 'Dataset 1',
+        //     data: Utils.numbers(NUMBER_CFG),
+        //     backgroundColor: Object.values(Utils.CHART_COLORS),
+        //   }
+        // ]
+      }
+    }
+  }
+}
+</script>
+
+
+
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <PieChart/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
